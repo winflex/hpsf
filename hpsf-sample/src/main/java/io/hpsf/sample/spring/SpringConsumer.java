f427f94493ea76d0f6b4175dafeea400d08fdcdc
lixiaohui <lixiaohui@agree.com.cn>
2019-07-16 14:22:33 +0800

    整理代码

--- hpsf-sample/src/main/java/io/hpsf/sample/spring/SpringConsumer.java
+++ hpsf-sample/src/main/java/io/hpsf/sample/spring/SpringConsumer.java
@@ -12,10 +12,13 @@ import io.hpsf.sample.api.AddService;
 public class SpringConsumer {
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-consumer.xml");
+		
+		// 接口调用
 		AddService service = (AddService) ctx.getBean("addService");
 		int result = service.add(1, 2);
 		System.out.println("invoke result: " + result);
 
+		//泛化调用
 		GenericService genericService = (GenericService) ctx.getBean("genericService");
 		int genericResult = (int) genericService.$invoke("add", new Class<?>[] { int.class, int.class }, new Object[] { 1, 2 });
 		System.out.println("genertic invoke result: " + genericResult);
